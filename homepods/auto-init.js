0798dec0e7d0ba5b19492cd186bc27d2d9c1a206
straw <nicsplatts@gmail.com>
2022-03-22 07:28:36 -0700

    Add files via upload

---
+++ homepods/auto-init.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+"use strict";(function(){try{var a=require("@marcom/ac-analytics");a.createBasicObserverSuite()}catch(e){}})();
+
+}
+/*
+     FILE ARCHIVED ON 03:52:37 Jun 09, 2020 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:43:59 Mar 15, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 377.211
+  exclusion.robots: 0.269
+  exclusion.robots.policy: 0.259
+  RedisCDXSource: 6.878
+  esindex: 0.014
+  LoadShardBlock: 134.132 (3)
+  PetaboxLoader3.datanode: 159.483 (5)
+  CDXLines.iter: 54.681 (3)
+  load_resource: 350.777 (2)
+  PetaboxLoader3.resolve: 221.199 (2)
+*/
